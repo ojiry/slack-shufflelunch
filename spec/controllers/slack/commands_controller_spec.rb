a5613d41a64dfd0b1c0ffd960d983cfc1d0f1c67
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-02-04 22:16:50 +0900

    Fix spec

--- spec/controllers/slack/commands_controller_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Slack::CommandsController, type: :controller do
-end
---
+++ spec/controllers/slack/slash_commands_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe Slack::SlashCommandsController, type: :controller do
+end
