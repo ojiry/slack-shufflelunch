110ac9cefafbcb886fe77bbf04436898100b48b2
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-02-04 15:43:31 +0900

    Add fake response

--- app/controllers/slack/commands_controller.rb
+++ app/controllers/slack/commands_controller.rb
@@ -1,5 +1,39 @@
 class Slack::CommandsController < ApplicationController
   def create
     logger.info params.inspect
+    render json: json
+  end
+
+  private
+
+  def json
+    {
+      "text": "Would you like to join the Suffle Lunch today?",
+      "attachments": [
+        {
+          "text": "Choose a game to play",
+          "fallback": "You are unable to choose a game",
+          "callback_id": "wopr_game",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "action",
+              "text": "Join",
+              "style": "primary",
+              "type": "button",
+              "value": "join"
+            },
+            {
+              "name": "action",
+              "text": "Leave",
+              "style": "danger",
+              "type": "button",
+              "value": "leave"
+            }
+          ]
+        }
+      ]
+    }.to_json
   end
 end
