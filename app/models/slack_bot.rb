3b1c8e899bae1eec8a8b68438366fc85521d02aa
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-02-12 00:11:33 +0900

    Add test message of bot

--- app/models/slack_bot.rb
+++ app/models/slack_bot.rb
@@ -6,7 +6,7 @@ class SlackBot
 
   def reply
     return unless mention?
-    Slack::Web::Client.new.chat_postMessage(channel: params[:event]['channel'], text: 'Hi!')
+    Slack::Web::Client.new.chat_postMessage(channel: params[:event]['channel'], attachments: test_message.to_json, as_user: true)
   end
 
   private
@@ -15,5 +15,48 @@ class SlackBot
 
   def mention?
     /\A<@#{user.slack_id}>/.match?(params[:event][:text])
+  end
+
+  def test_message
+    {
+      "text": "I am a test message http://slack.com",
+      "attachments": [
+        {
+          "text": "Please put join or leave button",
+          "fallback": "Your current Slack client doesn’t support Shuffle Lunch",
+          "callback_id": 1,
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "action",
+              "text": "Join",
+              "style": "primary",
+              "type": "button",
+              "value": "join"
+            },
+            {
+              "name": "action",
+              "text": "Leave",
+              "style": "danger",
+              "type": "button",
+              "value": "leave"
+            },
+            {
+              "name": "action",
+              "text": "Shuffle",
+              "type": "button",
+              "value": "shuffle",
+              "confirm": {
+                "title": "Are you sure?",
+                "text": "If you put Shuffle button, other members will be not able to entry lunch, right?",
+                "ok_text": "Yes",
+                "dismiss_text": "No"
+              }
+            }
+          ]
+        }
+      ]
+    }.to_json
   end
 end
