706821aa397ff2f73c7e02f50f50496553f21a98
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-02-13 05:29:39 +0900

    Hide shuffle button if users zero

--- app/models/interactive_component_builder.rb
+++ app/models/interactive_component_builder.rb
@@ -20,34 +20,7 @@ class InteractiveComponentBuilder
             "callback_id": lunch&.id,
             "color": "#3AA3E3",
             "attachment_type": "default",
-            "actions": [
-              {
-                "name": "action",
-                "text": "Join",
-                "style": "primary",
-                "type": "button",
-                "value": "join"
-              },
-              {
-                "name": "action",
-                "text": "Leave",
-                "style": "danger",
-                "type": "button",
-                "value": "leave"
-              },
-              {
-                "name": "action",
-                "text": "Shuffle",
-                "type": "button",
-                "value": "shuffle",
-                "confirm": {
-                  "title": "Are you sure?",
-                  "text": "If you put Shuffle button, other members will be not able to entry lunch, right?",
-                  "ok_text": "Yes",
-                  "dismiss_text": "No"
-                }
-              }
-            ]
+            "actions": actions
           }
         ]
       }
@@ -57,4 +30,37 @@ class InteractiveComponentBuilder
   private
 
   attr_reader :lunch
+
+  def actions
+    [].tap do |actions|
+      actions << {
+        "name": "action",
+        "text": "Join",
+        "style": "primary",
+        "type": "button",
+        "value": "join"
+      }
+      actions << {
+        "name": "action",
+        "text": "Leave",
+        "style": "danger",
+        "type": "button",
+        "value": "leave"
+      }
+      unless lunch.users.count.zero?
+        actions << {
+          "name": "action",
+          "text": "Shuffle",
+          "type": "button",
+          "value": "shuffle",
+          "confirm": {
+            "title": "Are you sure?",
+            "text": "If you put Shuffle button, other members will be not able to entry lunch, right?",
+            "ok_text": "Yes",
+            "dismiss_text": "No"
+          }
+        }
+      end
+    end
+  end
 end
