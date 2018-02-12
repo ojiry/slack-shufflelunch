7dd5c0418a5473d54deea42e2b902c5b38005b68
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2018-02-13 05:22:44 +0900

    Add uniqueness validation

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,4 +1,6 @@
 class GroupMember < ApplicationRecord
   belongs_to :group
   belongs_to :user
+
+  validates :group_id, uniqueness: { scope: :user_id }
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,6 @@
 class Participation < ApplicationRecord
   belongs_to :lunch
   belongs_to :user
+
+  validates :lunch_id, uniqueness: { scope: :user_id }
 end
